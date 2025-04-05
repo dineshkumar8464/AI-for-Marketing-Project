fa7fafbfde2093cd84446a7afa9a904fca1c52a0
Dinesh Kumar Manimela <136953450+dineshkumar8464@users.noreply.github.com>
2025-04-05 11:00:33 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv()
-# api_key = os.getenv("GOOGLE_API_KEY")
-
-# print("Your API Key:", api_key)  # Check if it's loaded properly
-
-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 print("API Key:", os.getenv("GEMINI_API_KEY"))
 print("API Key:", os.getenv("OPENAI_API_KEY"))
+print("API Key:", os.getenv("GOOGLE_SHEET_API_KEY"))
+print("Sheet ID:", os.getenv("GOOGLE_SHEET_ID"))
+print("Sheet Credentials path:", os.getenv("GOOGLE_SHEET_CREDENTIALS_PATH"))
+print("sheet name:", os.getenv("GOOGLE_SHEET_NAME"))
